43db69c1e9838343dd1c043dc39b2b28ec1d20ff
saishreddyk <saishreddy05@gmail.com>
2022-04-10 10:39:59 +0000

    Added main App.js code

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+// import NavContainer from './routes/drawer';
+import MainStack from './routes/mainStack';
+// import LoginScreen from './screens/loginScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <NavContainer />
+    // <LoginScreen />
+      <MainStack />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FF5236',
   },
 });
