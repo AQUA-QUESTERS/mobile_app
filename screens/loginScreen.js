936b0dcda25201efa5ec758092a5068b3753f083
SujithNimmala <sujithsainimmala@gmail.com>
2022-04-17 11:04:02 +0000

    minor changes

--- screens/loginScreen.js
+++ screens/loginScreen.js
@@ -77,7 +77,7 @@ const LoginScreen = ( {navigation, setSignedIn} ) => {
         }} >
             <View style={styles.container}>
                 <StatusBar style="inverted" />
-                <Text style={styles.logo}>Aqua-Questors</Text>
+                <Text style={styles.logo}>Aqua-Questers</Text>
                 <View style={styles.inputView}>
                     <TextInput
                         style={styles.inputText}
